a3e383aa1dcd09af35db40a7b0e0063a68919ca7
Tatyana Scotce <tscotce@gmail.com>
2018-02-26 23:21:44 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  string = programmer_hash[:dennis_ritchie][:languages]
+  string.first
 end
 
 def adding_matz
@@ -78,9 +81,12 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
 
-    
+     }
+programmer_hash[:yukihiro_matsumoto] = {
+               
+          }
+ 
 end
 
 def changing_alan
