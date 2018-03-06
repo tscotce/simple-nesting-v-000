b14ac6dc889225be4c95be29ec8970468c281d99
Tatyana Scotce <tscotce@gmail.com>
2018-03-06 03:14:23 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,8 @@ def adding_matz
         }
 
      }
-programmer_hash[:yukihiro_matsumoto] = {
-               
-          }
+programmer_hash[:yukihiro_matsumoto][:known_for] => "Ruby", :languages => ["LISP", "C"]
+    }
  
 end
 
@@ -107,7 +106,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"    
      
 end
 
@@ -127,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 end
